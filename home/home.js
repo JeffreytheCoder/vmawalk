ae951d7832a117a462bcf0ca1ba432c6fa03ab52
JeffreytheCoder <2397371802@qq.com>
2020-06-12 10:45:44 +0800

    commit v1.0.3

---
+++ home/home.js
@@ -0,0 +1,727 @@
+teachers = [{
+        "Chinese": "王赫",
+        "English": "Wang He"
+    },
+    {
+        "Chinese": null,
+        "English": "Maryann O'Brien"
+    },
+    {
+        "Chinese": "咸海明",
+        "English": "Maggie Xian"
+    },
+    {
+        "Chinese": "耿倩",
+        "English": "Melody Geng"
+    },
+    {
+        "Chinese": "杨永乐",
+        "English": "Eric Yang"
+    },
+    {
+        "Chinese": "伍仙洁",
+        "English": "Jenny Wu"
+    },
+    {
+        "Chinese": null,
+        "English": "Sabrina Waterfield"
+    },
+    {
+        "Chinese": "吴晓莉",
+        "English": "Shirley Wu"
+    },
+    {
+        "Chinese": "杜浩",
+        "English": "Howard Du"
+    },
+    {
+        "Chinese": "郑伟伟",
+        "English": "Simon Zheng"
+    },
+    {
+        "Chinese": "曹霞",
+        "English": "Cao Xia"
+    },
+    {
+        "Chinese": "秦星",
+        "English": "Ryan Qin"
+    },
+    {
+        "Chinese": "郭润尘",
+        "English": "Rayson Guo"
+    },
+    {
+        "Chinese": "谢瑾嵘",
+        "English": "Jean Xie"
+    },
+    {
+        "Chinese": "陈宇婷",
+        "English": "Christy Chen"
+    },
+    {
+        "Chinese": "陈燕静",
+        "English": "Eva Chen"
+    },
+    {
+        "Chinese": "谢燕萍",
+        "English": "Shelly Xie"
+    },
+    {
+        "Chinese": "唐芯雅",
+        "English": "Tang Xinya"
+    },
+    {
+        "Chinese": "王子豪",
+        "English": "Joel Wang"
+    },
+    {
+        "Chinese": "鲁燕",
+        "English": "Lu Yan"
+    },
+    {
+        "Chinese": "李桂敏",
+        "English": "Lisa Li"
+    },
+    {
+        "Chinese": "凌利红",
+        "English": "Ling Lihong"
+    },
+    {
+        "Chinese": "阮坤连",
+        "English": "Ruan Kunlian"
+    },
+    {
+        "Chinese": "朱晓军",
+        "English": "Zhu Xiaojun"
+    },
+    {
+        "Chinese": "张甲春",
+        "English": "Zhang Jiachun"
+    },
+    {
+        "Chinese": "王海燕",
+        "English": "Wang Haiyan"
+    },
+    {
+        "Chinese": "邵琼波",
+        "English": "Shao Qiongbo"
+    },
+    {
+        "Chinese": "孙艳萍",
+        "English": "Sun Yanping"
+    },
+    {
+        "Chinese": "陈燕琦",
+        "English": "Kay Chen"
+    },
+    {
+        "Chinese": "黄丽君",
+        "English": "June Huang"
+    },
+    {
+        "Chinese": "严欣然",
+        "English": "Sunny Yan"
+    },
+    {
+        "Chinese": "赵菲儿",
+        "English": "Zhao Feier"
+    },
+    {
+        "Chinese": "阳菲",
+        "English": "Yang Fei"
+    },
+    {
+        "Chinese": "吕羚",
+        "English": "Laura Lyu"
+    },
+    {
+        "Chinese": "陈俊昇",
+        "English": "Jun Chen"
+    },
+    {
+        "Chinese": null,
+        "English": "Nicholas Tan"
+    },
+    {
+        "Chinese": "周靖",
+        "English": "Dean Zhou"
+    },
+    {
+        "Chinese": "马骁",
+        "English": "Jack Ma"
+    },
+    {
+        "Chinese": "胡纯",
+        "English": "Michael Hu"
+    },
+    {
+        "Chinese": "张莉",
+        "English": "Lily Zhang"
+    },
+    {
+        "Chinese": "张媛",
+        "English": "Christina Zhang"
+    },
+    {
+        "Chinese": "崔玉",
+        "English": "Ella Cui"
+    },
+    {
+        "Chinese": "董芮",
+        "English": "Donna Dong"
+    },
+    {
+        "Chinese": "周怡辰",
+        "English": "Evelyn Zhou"
+    },
+    {
+        "Chinese": "张博",
+        "English": "Bryce Zhang"
+    },
+    {
+        "Chinese": "蒲妍冰",
+        "English": "Mia Pu"
+    },
+    {
+        "Chinese": "肖惠珊",
+        "English": "Sandara Xiao"
+    },
+    {
+        "Chinese": "杨锦婷",
+        "English": "Ivy Yang"
+    },
+    {
+        "Chinese": "龙增辉",
+        "English": "Allen Long"
+    },
+    {
+        "Chinese": "艾文玲",
+        "English": "Aileen Ai"
+    },
+    {
+        "Chinese": "周小伟",
+        "English": "Zhou Xiaowei"
+    },
+    {
+        "Chinese": "于世刚",
+        "English": "Yu Shigang"
+    },
+    {
+        "Chinese": "邓宁",
+        "English": "Deng Ning"
+    },
+    {
+        "Chinese": "陈潇",
+        "English": "Chablis Chen"
+    },
+    {
+        "Chinese": null,
+        "English": "Jayden  Chang"
+    },
+    {
+        "Chinese": null,
+        "English": "Paul Arnold Svaren"
+    },
+    {
+        "Chinese": "严健铭",
+        "English": "Ethan Yan"
+    },
+    {
+        "Chinese": "李函珏",
+        "English": "Li Hanjue"
+    },
+    {
+        "Chinese": "孙绿乔",
+        "English": "Nala Sun"
+    },
+    {
+        "Chinese": "刘宴妮",
+        "English": "Liu Yanni"
+    },
+    {
+        "Chinese": null,
+        "English": "Sean Huang"
+    },
+    {
+        "Chinese": null,
+        "English": "Angela Choi"
+    },
+    {
+        "Chinese": null,
+        "English": "Martin Kwok"
+    },
+    {
+        "Chinese": "黄茹茹",
+        "English": "Jane Huang"
+    },
+    {
+        "Chinese": "陈冬松",
+        "English": "Suzy Chen"
+    },
+    {
+        "Chinese": "黄瑜",
+        "English": "Yvonne Huang"
+    },
+    {
+        "Chinese": "周雅文",
+        "English": "Yawen Zhou"
+    },
+    {
+        "Chinese": "邬宁馨",
+        "English": "Mandy Wu"
+    },
+    {
+        "Chinese": "吴祎",
+        "English": "Wu Yi"
+    },
+    {
+        "Chinese": "罗晓",
+        "English": "Alice Luo"
+    },
+    {
+        "Chinese": "李思萦",
+        "English": "Li Siying"
+    },
+    {
+        "Chinese": "张愉婉",
+        "English": "Yvonne Zhang"
+    },
+    {
+        "Chinese": null,
+        "English": "Steve Gamble"
+    },
+    {
+        "Chinese": null,
+        "English": "Teisha Chung-Smith"
+    },
+    {
+        "Chinese": null,
+        "English": "Nicolas McNamara"
+    },
+    {
+        "Chinese": "王舒笛",
+        "English": "Crystal Wang"
+    },
+    {
+        "Chinese": "沈炜楠",
+        "English": "Irene Shen"
+    },
+    {
+        "Chinese": null,
+        "English": "Kirsten Nicole Saunders"
+    },
+    {
+        "Chinese": null,
+        "English": "Timbray Shafer"
+    },
+    {
+        "Chinese": null,
+        "English": "Isabel  Panadero"
+    },
+    {
+        "Chinese": null,
+        "English": "Donny Ng"
+    },
+    {
+        "Chinese": "祭菲",
+        "English": "Sophie Ji"
+    },
+    {
+        "Chinese": null,
+        "English": "Matthew Schroeder"
+    },
+    {
+        "Chinese": null,
+        "English": "Charlotte Bailey"
+    },
+    {
+        "Chinese": null,
+        "English": "Chesslyn Scott Bailey"
+    },
+    {
+        "Chinese": null,
+        "English": "Elizabeth Mitchell"
+    },
+    {
+        "Chinese": null,
+        "English": "Ashley Atkins"
+    },
+    {
+        "Chinese": "禹雅",
+        "English": "Olivia Yu"
+    },
+    {
+        "Chinese": null,
+        "English": "Joseph Grefer"
+    },
+    {
+        "Chinese": null,
+        "English": "Kimberly Garner"
+    },
+    {
+        "Chinese": "张新瑶",
+        "English": "Zhang Xinyao"
+    },
+    {
+        "Chinese": "黄春华",
+        "English": "Bridget Huang"
+    },
+    {
+        "Chinese": "王嘉熠",
+        "English": "Wang Jiayi"
+    },
+    {
+        "Chinese": null,
+        "English": "Michael Kwan"
+    },
+    {
+        "Chinese": "林锋洁",
+        "English": "Amber Lin"
+    },
+    {
+        "Chinese": null,
+        "English": "Ryan Bradwell"
+    },
+    {
+        "Chinese": "白凌",
+        "English": "Beryl Bai"
+    },
+    {
+        "Chinese": null,
+        "English": "James Mosca"
+    },
+    {
+        "Chinese": null,
+        "English": "Sarah Maffei"
+    },
+    {
+        "Chinese": null,
+        "English": "Gabriel Beadle"
+    },
+    {
+        "Chinese": null,
+        "English": "Michael Predmore"
+    },
+    {
+        "Chinese": "殷雁",
+        "English": "Yin Yan"
+    },
+    {
+        "Chinese": "张跞颖",
+        "English": "Zhang Luoying"
+    },
+    {
+        "Chinese": null,
+        "English": "Mogammad Zardad"
+    },
+    {
+        "Chinese": null,
+        "English": "Per Nilsson"
+    },
+    {
+        "Chinese": null,
+        "English": "Rothman Ng"
+    },
+    {
+        "Chinese": "龚禹",
+        "English": "Amy Gong"
+    },
+    {
+        "Chinese": "李维才",
+        "English": "Alex Li"
+    },
+    {
+        "Chinese": "尤晶磊",
+        "English": "Victoria You"
+    },
+    {
+        "Chinese": "孙靓雯",
+        "English": "Tina Sun"
+    },
+    {
+        "Chinese": "周幸茹",
+        "English": "Riva Zhou"
+    },
+    {
+        "Chinese": null,
+        "English": "Yassine Mahouch"
+    },
+    {
+        "Chinese": null,
+        "English": "Jacky Wong"
+    },
+    {
+        "Chinese": null,
+        "English": "Pamela Joseph"
+    },
+    {
+        "Chinese": null,
+        "English": "Nicholas Atwater"
+    },
+    {
+        "Chinese": null,
+        "English": "Ivan Tsang"
+    },
+    {
+        "Chinese": "高浩",
+        "English": "Gao Hao"
+    },
+    {
+        "Chinese": "马斌",
+        "English": "Matt Ma"
+    },
+    {
+        "Chinese": "徐源",
+        "English": "Randy Xu"
+    },
+    {
+        "Chinese": "何智伟",
+        "English": "He Zhiwei"
+    },
+    {
+        "Chinese": "吴国熹",
+        "English": "Hei Ng"
+    },
+    {
+        "Chinese": "杨文娟",
+        "English": "Vicky Yang"
+    },
+    {
+        "Chinese": null,
+        "English": "Amelita Tavera"
+    },
+    {
+        "Chinese": null,
+        "English": "Nichia Huxtable"
+    },
+    {
+        "Chinese": "叶元成",
+        "English": "Carter Ye"
+    },
+    {
+        "Chinese": "申磊",
+        "English": "Shen Lei"
+    },
+    {
+        "Chinese": "高升恒",
+        "English": "Raphael Gao"
+    },
+    {
+        "Chinese": null,
+        "English": "Jason Hum"
+    },
+    {
+        "Chinese": "吴邵汐",
+        "English": "Shawn Wu"
+    },
+    {
+        "Chinese": "李戈",
+        "English": "Andy Li"
+    },
+    {
+        "Chinese": "李钰",
+        "English": "Nancy Li"
+    },
+    {
+        "Chinese": null,
+        "English": "Roland Wang"
+    },
+    {
+        "Chinese": "刘洋",
+        "English": "Christian Liu"
+    },
+    {
+        "Chinese": "蔡润苑",
+        "English": "Joanna Cai"
+    },
+    {
+        "Chinese": "潘游美忆",
+        "English": "May Pan"
+    },
+    {
+        "Chinese": null,
+        "English": "Leon Tkachyk"
+    },
+    {
+        "Chinese": "段炼",
+        "English": "Eric Duan"
+    },
+    {
+        "Chinese": "许美杰",
+        "English": "Vincent Xu"
+    },
+    {
+        "Chinese": "王大伟",
+        "English": "Wang Dawei"
+    },
+    {
+        "Chinese": "吴惠洋",
+        "English": "Michael Wu"
+    },
+    {
+        "Chinese": "徐亮",
+        "English": "Tony Xu"
+    },
+    {
+        "Chinese": "饶琼",
+        "English": "Joan Rao"
+    },
+    {
+        "Chinese": null,
+        "English": "Cesar Sousa"
+    },
+    {
+        "Chinese": "石忞旻",
+        "English": "Shi Minmin"
+    },
+    {
+        "Chinese": "徐志伟",
+        "English": "Sam Xu"
+    },
+    {
+        "Chinese": "张泽泳",
+        "English": "Scott Zhang"
+    },
+    {
+        "Chinese": "林文鑫",
+        "English": "Cavan Lin"
+    },
+    {
+        "Chinese": "贾如愚",
+        "English": "Rain Jia"
+    },
+    {
+        "Chinese": null,
+        "English": "Dixie Beadle"
+    },
+    {
+        "Chinese": "邱野",
+        "English": "Leo Qiu"
+    },
+    {
+        "Chinese": "马梦",
+        "English": "Martina Ma"
+    },
+    {
+        "Chinese": null,
+        "English": "Brigid Transon"
+    },
+    {
+        "Chinese": null,
+        "English": "Michael Tsalka"
+    },
+    {
+        "Chinese": null,
+        "English": "Benjamin Fleischacker"
+    },
+    {
+        "Chinese": null,
+        "English": "Leonard Lindweld"
+    },
+    {
+        "Chinese": "曹耘",
+        "English": "Susan Cao"
+    },
+    {
+        "Chinese": "王砥",
+        "English": "Wang Di"
+    },
+    {
+        "Chinese": "吴佳宜",
+        "English": "Chloe Wu"
+    },
+    {
+        "Chinese": "林素琴",
+        "English": "Jackie Lin"
+    },
+    {
+        "Chinese": "屠旖旎",
+        "English": "Vivian Tu"
+    },
+    {
+        "Chinese": null,
+        "English": "Louie Cabognason"
+    },
+    {
+        "Chinese": "梁晶玮",
+        "English": "Kenvi Liang"
+    },
+    {
+        "Chinese": "白东亮",
+        "English": "James Bai"
+    },
+    {
+        "Chinese": "赖雨青",
+        "English": "Lavita Lai"
+    },
+    {
+        "Chinese": null,
+        "English": "Colton Francis"
+    },
+    {
+        "Chinese": "陈逸秋",
+        "English": "Chen Yiqiu"
+    },
+    {
+        "Chinese": "张子提",
+        "English": "Daniel Zhang"
+    },
+    {
+        "Chinese": "吴晓萍",
+        "English": "Lilia Wu"
+    },
+    {
+        "Chinese": "王思懿",
+        "English": "Valentina Wang"
+    },
+    {
+        "Chinese": "李扬",
+        "English": "Li Yang"
+    }
+]
+
+function fuzzyQuery() {
+    var keyword = document.getElementById('keyword').value;
+    var list = teachers;
+    var result = []
+    var success = false;
+    for (var i = 0; i < list.length; i++) {
+        // console.log(typeof(list[i]))
+        // console.log(typeof(list[i].Chinese))
+        if (list[i].Chinese == null) {
+            continue;
+        }
+        if (list[i].Chinese.indexOf(keyword) >= 0) {
+            result.push(list[i].Chinese)
+            var success = true;
+        }
+        if (list[i].English.indexOf(keyword) >= 0) {
+            result.push(list[i].English)
+            var success = true;
+        }
+    }
+    if (success == false) {
+        console.log("not found")
+    }
+    console.log(result)
+}
+
+layui.config({
+    base: '../layui/', // 自己autocomplete文件路径
+    version: false,
+    debug: false,
+});
+
+layui.use(['jquery', 'autocomplete'], function() {
+    var $ = layui.jquery,
+        autocomplete = layui.autocomplete;
+    autocomplete.render({
+        elem: $('#keyword'),
+        cache: true,
+        url: 'http://localhost:250/C%3A/Users/23973/Desktop/vmawalk/files/Teachers.json',
+        response: { Chinese: 'Chinese', English: 'English' },
+        onselect: function(resp) {
+            $('#content1').html("NEW RENDER: " + JSON.stringify(resp));
+        }
+    })
+})
