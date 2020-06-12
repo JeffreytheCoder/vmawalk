c28f028c025db44a23030c077134a07bc002bad8
JeffreytheCoder <2397371802@qq.com>
2020-06-12 17:27:15 +0800

    w

--- search/search.js
+++ search/search.js
@@ -1,45 +1,37 @@
 window.onload = function() {
-        var query = window.location.search.substring(1);
-        console.log(query);
-        var keyword = getUrlQueryString('q');
-        console.log(keyword);
-        var result = fuzzyQuery(keyword);
-    },
+    var keyword = getUrlQueryString();
+    console.log("haha");
+    alert("what");
+};
 
-    function getUrlQueryString(names, urls) {
-        var query = window.location.search.substring(1);
-        // 把参数按&拆分成数组
-        var param_arr = query.split("&");
-        for (var i = 0; i < param_arr.length; i++) {
-            var pair = param_arr[i].split("=");
-            if (pair[0] == name) {
-                return pair[1];
-            }
-        }
-        return (false);
-    },
+function getUrlQueryString() {
+    var equal = window.location.href.indexOf("=")
+    var query = window.location.href.substring(equal);
+    console.log(query)
+    return query;
+};
 
-    function fuzzyQuery(keyword) {
-        var list = teachers;
-        var result = []
-        var success = false;
-        for (var i = 0; i < list.length; i++) {
-            // console.log(typeof(list[i]))
-            // console.log(typeof(list[i].Chinese))
-            if (list[i].Chinese == null) {
-                continue;
-            }
-            if (list[i].Chinese.indexOf(keyword) >= 0) {
-                result.push(list[i].Chinese)
-                var success = true;
-            }
-            if (list[i].English.indexOf(keyword) >= 0) {
-                result.push(list[i].English)
-                var success = true;
-            }
+function fuzzyQuery(keyword) {
+    var list = teachers;
+    var result = []
+    var success = false;
+    for (var i = 0; i < list.length; i++) {
+        // console.log(typeof(list[i]))
+        // console.log(typeof(list[i].Chinese))
+        if (list[i].Chinese == null) {
+            continue;
         }
-        if (success == false) {
-            console.log("not found")
+        if (list[i].Chinese.indexOf(keyword) >= 0) {
+            result.push(list[i].Chinese)
+            var success = true;
         }
-        return result;
+        if (list[i].English.indexOf(keyword) >= 0) {
+            result.push(list[i].English)
+            var success = true;
+        }
     }
+    if (success == false) {
+        console.log("not found")
+    }
+    return result;
+}
