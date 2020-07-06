895bb5fc8bc06dd2b8052c8ec3f23ba31b6ca461
JeffreytheCoder <2397371802@qq.com>
2020-07-06 21:13:51 +0800

    1

--- menu/menu.js
+++ menu/menu.js
@@ -1,51 +1,92 @@
-console.log(decodeURI(window.location.href));
-var query = "1-105" //getUrlQueryString(decodeURI(window.location.href));
-id = query.substring(2)
-
 function getUrlQueryString() {
     var equal = window.location.href.indexOf("=")
     var query = window.location.href.substring(equal + 1);
     return query;
 };
 
-layui.use(["jquery", "layer"], function() {
-    var $ = layui.$;
-    var url = "";
-    var teacher = null;
+//global variable
+var teacherObj;
+var coursesList;
+var courseObj;
+var courseList;
 
-    data = {}
-    if (query[0] == "1") {
-        $.get("https://vmawalk.azurewebsites.net/api/teacher/"+id,function (result) {
-            teacher = result;
-            console.log(teacher)
-        });
-        url = "https://vmawalk.azurewebsites.net/api/course/GetWithTeachers";
-        data.id = Number(id);
-    } else if (query[0] == "2") {
-        url = "https://vmawalk.azurewebsites.net/api/course/GetWithCode";
-        data.code = id;
-    }
+function callData(query, callback) {
+    console.log(456);
+    layui.use(["jquery", "layer"], function() {
+        var $ = layui.$;
+        var url = "";
+        var teacher = null;
 
-    $.ajax({
-        type: "GET",
-        url: url,
-        contentType: "application/json",
-        data: data,
-        success: function(req) {
-            console.log(req);
-        },
-        error: function(req) {
-            console.log(req);
+        data = {}
+        if (query[0] == "1") {
+            $.get("https://vmawalk.azurewebsites.net/api/teacher/" + id, function(result) {
+                teacher = result;
+                teacherObj = teacher;
+                console.log(teacher)
+            });
+            url = "https://vmawalk.azurewebsites.net/api/course/GetWithTeachers";
+            data.id = Number(id);
+        } else if (query[0] == "2") {
+            url = "https://vmawalk.azurewebsites.net/api/course/GetWithCode";
+            data.code = id;
         }
-    });
-})
 
-//动态加载
-//图片/课程代码、老师/课名称
-//每个profile里：课程代码/老师图片、课/老师名称、五个评分、最佳评论
-var namewithpic = document.getElementById("namewithpic");
-var name = document.createElement("label");
-var label_id = document.createAttribute("id");
-label_id.nodeValue = id;
-name.innerText = "Atwater";
-namewithpic.appendChild(name);
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: "application/json",
+            data: data,
+            success: function(req) {
+                callback();
+                console.log(req);
+            },
+            error: function(req) {
+                callback();
+                console.log(req);
+                return req;
+            }
+        });
+    })
+}
+
+window.onload = function() {
+
+    //init
+    console.log(decodeURI(window.location.href));
+    var query = "1-100" //getUrlQueryString(decodeURI(window.location.href));
+    id = query.substring(2)
+
+    callData(query, function() {
+        console.log(123);
+        if (query[0] == "1") {
+            //namewithpic
+            namewithpic = document.getElementById("namewithpic");
+
+            var image = document.createElement("div");
+            image.style.cssText = 'background-image: url(https://github.com/JeffreytheCoder/vmawalk/blob/master/img/wanghe.jpg?raw=true);';
+            image.className = "image";
+            namewithpic.appendChild(image);
+
+            var name = document.createElement("h2");
+            name.innerHTML = "<strong>" + teacherObj.chineseName + " " + teacherObj.englishName + "</strong>";
+            namewithpic.appendChild(name);
+
+            len = 3;
+            for (i = 0; i < len; i++) {
+                var image = document.createElement("div");
+            }
+
+        }
+
+        if (query[0] == "2") {
+            //namewithpic
+            namewithpic = document.getElementById("namewithpic");
+
+            var code = document.createElement("div");
+            code.style.cssText = "background-color: #69BDC8;";
+            code.innerHTML = "<font color='white'>MATH502</font>";
+            code.className = "code";
+            namewithpic.appendChild(code);
+        }
+    })
+}
