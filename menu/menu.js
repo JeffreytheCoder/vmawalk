54f133664663d6c47e34c954684a9214ef1b181d
JeffreytheCoder <2397371802@qq.com>
2020-07-14 17:10:03 +0800

    add header title

--- menu/menu.js
+++ menu/menu.js
@@ -54,198 +54,206 @@ function callData(query, queryID, callback) {
     })
 }
 
+function loadTeacherMenu() {
+    //add namewithpic
+    namewithpic = document.getElementById("namewithpic");
+
+    var image = document.createElement("div");
+    var imageURL = Imagelink[teacherObj.id];
+    image.style.cssText = 'background-image: url(' + imageURL + ');';
+    console.log(imageURL);
+    image.className = "image";
+    namewithpic.appendChild(image);
+    var teacherName = document.createElement("h2");
+    var teacherNameText = [teacherObj.chineseName, teacherObj.englishName].join(" ").trim()
+    teacherName.innerHTML = "<strong>" + teacherNameText + "</strong>";
+    namewithpic.appendChild(teacherName);
+
+    //add header title
+    document.title = teacherNameText + " | Vmawalk";
+
+    // add courseframe
+    var courseFrame = document.getElementById("course-frame")
+
+    /**
+     * @type {{courses:{id:Number,courseName:string,courseCode:string,teacherId:number,averageScore:string}[],
+     * text:{courseId:number,text:string}[]
+     * }} courseObj
+     */
+
+    var courseObj = courseList;
+
+    courseList = courseObj.courses;
+
+    var reviewList = courseObj.text
+    console.log(reviewList)
+
+    courseList.forEach(
+        course => {
+            var scoreList = ["N/A", "N/A", "N/A", "N/A", "N/A"],
+                bestReview = "No Review",
+                queryLink = "../profile/profile.html?query=" + course.id + "";
+            if (course.averageScore != null) {
+                scoreList = course.averageScore.split("|");
+            }
+            var review = reviewList.find(review =>
+                review.courseId == course.id
+            )
+            if (review != undefined) {
+                bestReview = review.text;
+            }
+
+            var courseElement = document.createElement("div");
+            courseElement.className = "course";
+            courseElement.innerHTML = `<br>
+<table>
+<tr>
+<td width="200px">
+    <a href="` + queryLink + `" class="profile-link">
+        <div class="icon-round">` + course.courseCode + `</div>
+        <div class="course-name">
+            <font color="black" size="3">` + course.courseName + `</font><br />
+            <font color="#69BDC8" size="2">Full Profile ></font>
+        </div>
+    </a>
+</td>
+<td class="rating-cell">
+<font size="5" color="black">` + scoreList[0] + `</font><br /> Overall
+</td>
+<td class="rating-cell">
+    <font size="5" color="black">` + scoreList[1] + `</font><br /> Easiness
+</td>
+<td class="rating-cell">
+    <font size="5" color="black">` + scoreList[2] + `</font><br /> Workload
+</td>
+<td class="rating-cell">
+    <font size="5" color="black">` + scoreList[3] + `</font><br /> Clarity
+</td>
+<td class="rating-cell">
+    <font size="5" color="black">` + scoreList[4] + `</font><br /> Helpfulness
+</td>
+<td width="200px" style="text-align: center; margin-left: 10px;">` + bestReview + `</td>
+</tr>
+</table>
+<br>`;
+            courseFrame.appendChild(courseElement);
+        }
+    )
+}
+
+function loadCourseMenu() {
+    //add namewithpic
+    namewithpic = document.getElementById("namewithpic");
+
+    var code = document.createElement("div");
+    code.style.cssText = "background-color: #69BDC8;";
+    code.innerHTML = "<font color='white'>" + queryID + "</font>";
+    code.className = "code";
+    namewithpic.appendChild(code);
+
+    var courseName = document.createElement("h2");
+
+    var courseObj = courseList;
+    courseList = courseObj.courses;
+    console.log(courseList)
+
+    var courseNameText = courseList[0].courseName;
+
+    courseName.innerHTML = "<strong>" + courseNameText + "</strong>";
+    namewithpic.appendChild(courseName);
+    //add header title
+    document.title = courseNameText + " | Vmawalk";
+
+    // add courseframe
+    var courseFrame = document.getElementById("course-frame")
+    var teacherNameList = {};
+
+    // add teachers' name into the list
+    courseList.forEach(
+        course => {
+            // find teacher with id
+            var teacher = teachers.find(teacher => teacher.id === course.teacherId)
+                // parse the teacher name
+            teacherNameList[teacher.id] = [teacher.chineseName, teacher.englishName].join(" ").trim()
+        }
+    )
+
+    var reviewList = courseObj.text
+    console.log(reviewList)
+
+    courseList.forEach(
+        course => {
+            //prepare score list, best review, and image link
+            var scoreList = ["N/A", "N/A", "N/A", "N/A", "N/A"],
+                bestReview = "No Review",
+                queryLink = "https://jeffreythecoder.github.io/vmawalk/profile/profile?query=" + course.id + "";
+            if (course.averageScore != null) {
+                scoreList = course.averageScore.split("|");
+            }
+            var review = reviewList.find(review =>
+                review.courseId == course.id
+            )
+            if (review != undefined) {
+                bestReview = review.text;
+            }
+            var imageURL = Imagelink[course.teacherId];
+
+            var courseElement = document.createElement("div");
+            courseElement.className = "course";
+            courseElement.innerHTML = `<br>
+<table>
+<tr>
+<td width="90px">
+<a href="` + queryLink + `">
+<div class="icon-round" style="background-image: url(` + imageURL + `)></div>
+</a>
+</td>
+<td width="110px">
+<a href="` + queryLink + `" style="text-decoration: none;">
+<font color="black" size="3">` + teacherNameList[course.teacherId] + `</font><br />
+<font color="#69BDC8" size="2">Full Profile ></font>
+</a>
+<td class="rating-cell">
+<font size="5" color="black">` + scoreList[0] + `</font><br /> Overall
+</td>
+<td class="rating-cell">
+<font size="5" color="black">` + scoreList[1] + `</font><br /> Easiness
+</td>
+<td class="rating-cell">
+<font size="5" color="black">` + scoreList[2] + `</font><br /> Workload
+</td>
+<td class="rating-cell">
+<font size="5" color="black">` + scoreList[3] + `</font><br /> Clarity
+</td>
+<td class="rating-cell">
+<font size="5" color="black">` + scoreList[4] + `</font><br /> Helpfulness
+</td>
+<td width="200px">` + bestReview + `</td>
+</tr>
+</table>
+<br>`;
+            courseFrame.appendChild(courseElement);
+        }
+    )
+}
+
 window.onload = function() {
 
     //init
     var query = getUrlQueryString(decodeURI(window.location.href));
     console.log(query);
-    queryID = query.substring(2)
+    queryID = query.substring(2);
 
     loadHeader();
-
     callData(query, queryID, function() {
         count++;
         console.log(count);
         if (count == 2) {
             if (query[0] == "1") {
-                //add namewithpic
-                namewithpic = document.getElementById("namewithpic");
-
-                var image = document.createElement("div");
-                var imageURL = Imagelink[teacherObj.id];
-                image.style.cssText = 'background-image: url(' + imageURL + ');';
-                console.log(imageURL);
-                image.className = "image";
-                namewithpic.appendChild(image);
-                var teacherName = document.createElement("h2");
-                teacherName.innerHTML = "<strong>" + [teacherObj.chineseName, teacherObj.englishName].join(" ").trim() + "</strong>";
-                namewithpic.appendChild(teacherName);
-                //add header title
-                document.title = teacherName;
-
-                // add courseframe
-                var courseFrame = document.getElementById("course-frame")
-
-                /**
-                 * @type {{courses:{id:Number,courseName:string,courseCode:string,teacherId:number,averageScore:string}[],
-                 * text:{courseId:number,text:string}[]
-                 * }} courseObj
-                 */
-
-                var courseObj = courseList;
-
-                courseList = courseObj.courses;
-
-                var reviewList = courseObj.text
-                console.log(reviewList)
-
-                courseList.forEach(
-                    course => {
-                        var scoreList = ["N/A", "N/A", "N/A", "N/A", "N/A"],
-                            bestReview = "No Review",
-                            queryLink = "../profile/profile.html?query=" + course.id + "";
-                        if (course.averageScore != null) {
-                            scoreList = course.averageScore.split("|");
-                        }
-                        var review = reviewList.find(review =>
-                            review.courseId == course.id
-                        )
-                        if (review != undefined) {
-                            bestReview = review.text;
-                        }
-
-                        var courseElement = document.createElement("div");
-                        courseElement.className = "course";
-                        courseElement.innerHTML = `<br>
-    <table>
-        <tr>
-            <td width="200px">
-                <a href="` + queryLink + `" class="profile-link">
-                    <div class="icon-round">` + course.courseCode + `</div>
-                    <div class="course-name">
-                        <font color="black" size="3">` + course.courseName + `</font><br />
-                        <font color="#69BDC8" size="2">Full Profile ></font>
-                    </div>
-                </a>
-            </td>
-            <td class="rating-cell">
-            <font size="5" color="black">` + scoreList[0] + `</font><br /> Overall
-            </td>
-            <td class="rating-cell">
-                <font size="5" color="black">` + scoreList[1] + `</font><br /> Easiness
-            </td>
-            <td class="rating-cell">
-                <font size="5" color="black">` + scoreList[2] + `</font><br /> Workload
-            </td>
-            <td class="rating-cell">
-                <font size="5" color="black">` + scoreList[3] + `</font><br /> Clarity
-            </td>
-            <td class="rating-cell">
-                <font size="5" color="black">` + scoreList[4] + `</font><br /> Helpfulness
-            </td>
-            <td width="200px" style="text-align: center; margin-left: 10px;">` + bestReview + `</td>
-        </tr>
-    </table>
-    <br>`;
-                        courseFrame.appendChild(courseElement);
-                    }
-                )
+                loadTeacherMenu();
             }
-
             if (query[0] == "2") {
-                //add namewithpic
-                namewithpic = document.getElementById("namewithpic");
-
-                var code = document.createElement("div");
-                code.style.cssText = "background-color: #69BDC8;";
-                code.innerHTML = "<font color='white'>" + queryID + "</font>";
-                code.className = "code";
-                namewithpic.appendChild(code);
-
-                var courseName = document.createElement("h2");
-
-                var courseObj = courseList;
-                courseList = courseObj.courses;
-                console.log(courseList)
-
-                var courseNameText = courseList[0].courseName;
-
-                courseName.innerHTML = "<strong>" + courseNameText + "</strong>";
-                namewithpic.appendChild(courseName);
-                //add header title
-                document.title = courseNameText;
-
-                // add courseframe
-                var courseFrame = document.getElementById("course-frame")
-                var teacherNameList = {};
-
-                // add teachers' name into the list
-                courseList.forEach(
-                    course => {
-                        // find teacher with id
-                        var teacher = teachers.find(teacher => teacher.id === course.teacherId)
-                            // parse the teacher name
-                        teacherNameList[teacher.id] = [teacher.chineseName, teacher.englishName].join(" ").trim()
-                    }
-                )
-
-                var reviewList = courseObj.text
-                console.log(reviewList)
-
-                courseList.forEach(
-                    course => {
-                        //prepare score list, best review, and image link
-                        var scoreList = ["N/A", "N/A", "N/A", "N/A", "N/A"],
-                            bestReview = "No Review",
-                            queryLink = "https://jeffreythecoder.github.io/vmawalk/profile/profile?query=" + course.id + "";
-                        if (course.averageScore != null) {
-                            scoreList = course.averageScore.split("|");
-                        }
-                        var review = reviewList.find(review =>
-                            review.courseId == course.id
-                        )
-                        if (review != undefined) {
-                            bestReview = review.text;
-                        }
-                        var imageURL = Imagelink[course.teacherId];
-
-                        var courseElement = document.createElement("div");
-                        courseElement.className = "course";
-                        courseElement.innerHTML = `<br>
-        <table>
-            <tr>
-                <td width="90px">
-                    <a href="` + queryLink + `">
-                    <div class="icon-round" style="background-image: url(` + imageURL + `)></div>
-                    </a>
-                </td>
-                <td width="110px">
-                    <a href="` + queryLink + `" style="text-decoration: none;">
-                        <font color="black" size="3">` + teacherNameList[course.teacherId] + `</font><br />
-                        <font color="#69BDC8" size="2">Full Profile ></font>
-                    </a>
-                    <td class="rating-cell">
-                        <font size="5" color="black">` + scoreList[0] + `</font><br /> Overall
-                    </td>
-                    <td class="rating-cell">
-                        <font size="5" color="black">` + scoreList[1] + `</font><br /> Easiness
-                    </td>
-                    <td class="rating-cell">
-                        <font size="5" color="black">` + scoreList[2] + `</font><br /> Workload
-                    </td>
-                    <td class="rating-cell">
-                        <font size="5" color="black">` + scoreList[3] + `</font><br /> Clarity
-                    </td>
-                    <td class="rating-cell">
-                        <font size="5" color="black">` + scoreList[4] + `</font><br /> Helpfulness
-                    </td>
-                    <td width="200px">` + bestReview + `</td>
-            </tr>
-        </table>
-        <br>`;
-                        courseFrame.appendChild(courseElement);
-                    }
-                )
+                loadCourseMenu();
             }
             loadFooter();
         }
