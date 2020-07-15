a07c7e6cda559734c6e556279b4b187cc068c3d8
JeffreytheCoder <2397371802@qq.com>
2020-07-15 12:07:11 +0800

    add header for about and team

--- headerAndFooter/headerAndFooter.js
+++ headerAndFooter/headerAndFooter.js
@@ -25,7 +25,11 @@ function loadHeader() {
             <option value="">请选择课程和老师</option>
         </select>
     </div>
-    <button class="layui-btn layui-btn-fluid login-btn" lay-submit lay-filter="submit" style="width: 70px; text-align: center; background-color: #0098ac;">搜索</button>
+    <button class="layui-btn layui-btn-fluid login-btn" lay-submit lay-filter="submit" style="width: 70px; text-align: center; background-color: #0098ac;     box-shadow: /* -7px -7px 20px 0px #fff9, */
+    /* -4px -4px 5px 0px #fff9, */
+    7px 7px 20px 0px #0002, 4px 4px 5px 0px #0001, /* inset 0px 0px 0px 0px #fff9, */
+    inset 0px 0px 0px 0px #0001, /* inset 0px 0px 0px 0px #fff9, */
+    inset 0px 0px 0px 0px #0001;">搜索</button>
 </form>
 <div style="display: flex;">
     <div style="padding-right: 20px">
