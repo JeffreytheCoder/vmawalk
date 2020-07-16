858ad680625bf453a486442183f5a82ab001d670
JeffreytheCoder <2397371802@qq.com>
2020-07-16 18:12:56 +0800

    myreview

--- myreview/myreview.js
+++ myreview/myreview.js
@@ -1,27 +1,32 @@
-layui.use["jquery"] {
-    function getUserReviews() {
-        /**
-         * @type {JQueryStatic}
-         */
-        var $ = layui.$;
+//global variable
+var reviewObj;
 
-        $.get("http://vma-walk.azurewebsites.net/api/Reviews/GetUserReviews",
-            /**
-             * @param {{
-             * id:number,
-             * userId:number,
-             * courseId:number,
-             * teacherId:number,
-             * year:number,
-             * semester:boolean,
-             * grade:string,
-             * score:string,
-             * text:string
-             * }[]} data - 课程类型
-             */
-            function (data) {
-                console.log(data)
-            })
+function getUserReviews() {
+    /**
+     * @type {JQueryStatic}
+     */
+    var $ = layui.$;
+
+    $.get("http://vma-walk.azurewebsites.net/api/Reviews/GetUserReviews",
+        /**
+         * @param {{
+         * id:number,
+         * userId:number,
+         * courseId:number,
+         * teacherId:number,
+         * year:number,
+         * semester:boolean,
+         * grade:string,
+         * score:string,
+         * text:string
+         * }[]} data - 课程类型
+         */
+        function(data) {
+            reviewObj = data;
+            console.log(data)
+        })
+}
+
 function loadingChange() {
     if (document.readyState == "complete") {
         loadHeader();
