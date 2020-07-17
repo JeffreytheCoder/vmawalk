d7bc513dd9aea8eac4fd7f93efd8e6d6bf85e935
JeffreytheCoder <2397371802@qq.com>
2020-07-17 23:46:00 +0800

    trim

--- myreview/myreview.js
+++ myreview/myreview.js
@@ -79,7 +79,7 @@ function loadReview() {
         //convert insertDate
         var date = reviewList[i].insertDate.split("T")[0];
         // get scores
-        var scoreList = reviewList[i].score.trimend().split("|");
+        var scoreList = reviewList[i].score.trim().split("|");
 
         // add a review block
         reviewDiv = document.getElementById("review-div");
