7ea6639d2d3e0010eed39467006ac2a37f304a40
弘韬 张 <hongtao_zhang@outlook.com>
2020-07-20 15:57:39 +0800

    optimize review and add the support for default course

--- review/review.js
+++ review/review.js
@@ -19,45 +19,58 @@ layui.use("layer", function () {
 window.onload = function () {
     layui.use(['layer', 'jquery', 'form'], function () {
 
-        var $ = layui.jquery;
+        /**
+         * @type {JQueryStatic}
+         */
+        var $ = layui.jquery,
+            form = layui.form;
 
-        for (var i = 1; i < teachers.length; i++) {
-            if (teachers[i].chineseName == null) {
-                $("#teacher").append(new Option(teachers[i].englishName, teachers[i].id));
-            }
+        teachers.filter(teacher => {
+            if (teacher.chineseName == null) {
+                $("#teacher").append(new Option(teacher.englishName, teacher.id))
+
+                return false;
+            } else
+                return true
+        }).forEach(teacher =>
+            $("#teacher").append(new Option([teacher.chineseName, teacher.englishName].join(" "), teacher.id))
+        )
+
+        // default value
+        if (location.search.length != 0) {
+            var course = CoursesWithTeacher.find(x => x.id === Number(
+                location.search.substr(1).split("&")[0].split("=")[1]))
+
+            $("#teacher").val(course.teacherId)
+            CoursesWithTeacher.forEach(
+                _course => {
+                    if (_course.teacherId === course.teacherId)
+                        $("#course").append(new Option([_course.courseName, _course.courseCode].join(" "), _course.id))
+                }
+            )
+            $('#course').val(course.id)
+
         }
 
-        for (var i = 1; i < teachers.length; i++) {
-            if (teachers[i].chineseName != null) {
-                $("#teacher").append(new Option(teachers[i].chineseName + " " + teachers[i].englishName, teachers[i].id));
-            }
-        }
+        form.render('select');
 
-        layui.form.render('select');
+        // listener of select
+        form.on('select(teacher)', function (data) {
+            let obj = document.getElementById("course");
+            for (i = obj.options.length - 1; i >= 1; i--) {
+                obj.options[i] = null;
+            }
+            var teacher_id = data.value;
+            for (var i = 1; i < CoursesWithTeacher.length; i++) {
+                if (CoursesWithTeacher[i].teacherId == teacher_id) {
+                    $("#course").append(new Option(CoursesWithTeacher[i].courseCode + " " + CoursesWithTeacher[i].courseName, CoursesWithTeacher[i].id));
+                }
+            }
+            form.render("select");
+        })
 
     })
 }
-
-layui.use(['layer', 'jquery', 'form'], function () {
-
-    var layer = layui.layer,
-        $ = layui.jquery,
-        form = layui.form;
-
-    form.on('select(teacher)', function (data) {
-        obj = document.getElementById("course");
-        for (i = obj.options.length - 1; i >= 1; i--) {
-            obj.options[i] = null;
-        }
-        var teacher_id = data.value;
-        for (var i = 1; i < CoursesWithTeacher.length; i++) {
-            if (CoursesWithTeacher[i].teacherId == teacher_id) {
-                $("#course").append(new Option(CoursesWithTeacher[i].courseCode + " " + CoursesWithTeacher[i].courseName, CoursesWithTeacher[i].id));
-            }
-        }
-        layui.form.render("select");
-    })
-})
 
 layui.use(['form', 'jquery', 'layer'], function () {
     var form = layui.form,
@@ -70,7 +83,13 @@ layui.use(['form', 'jquery', 'layer'], function () {
         var articleSummary = data.field.articleSummary;
         console.log(data);
 
-        var scores = [data.field.overall, data.field.easiness, data.field.workload, data.field.clarity, data.field.helpfulness].join("|");
+        var scores = [
+            data.field.overall,
+            data.field.easiness,
+            data.field.workload,
+            data.field.clarity,
+            data.field.helpfulness
+        ].join("|");
 
         console.log(JSON.stringify({
             teacherId: data.field.teacher,
