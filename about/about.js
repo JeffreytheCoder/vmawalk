320fd08b1dc2551d2d029ddedde4a4646289f708
弘韬 张 <hongtao_zhang@outlook.com>
2020-08-03 17:12:50 +0800

    fix an stupid error....

--- about/about.js
+++ about/about.js
@@ -6,7 +6,7 @@ function loadingChange() {
             var $ = layui.$;
             document.documentElement.clientWidth > 750 ? $(".mobile").css("display", "none") : $(".standard").css("display", "none");
 
-            window.onresize() = function () {
+            window.onresize = function () {
                 if (document.documentElement.clientWidth > 750) {
                     $(".standard").css("display", "block")
                     $(".mobile").css("display", "none")
